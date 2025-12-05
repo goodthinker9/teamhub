6447f35bd1e634af3c889aa5c410e1e86a21fd55
goodthinker9 <kalidmmohammed8@gmail.com>
2025-12-05 16:18:01 +0300

    second commit

--- team-project/javascript/script.js
+++ team-project/javascript/script.js
@@ -4,3 +4,33 @@ const navLinks = document.getElementById('navLinks');
 menuBtn.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
+const ntnBtn = document.getElementById('btn');
+const departInput = document.getElementById('depart');
+const returnInput = document.getElementById('return');
+const adultsInput = document.getElementById('Adults');
+const childrenInput = document.getElementById('Children');
+const optInput = document.getElementById('option');
+const resultDiv = document.getElementById('result');
+
+ntnBtn.addEventListener('dblclick', () =>{
+departInput.value = '';
+returnInput.value = '';
+adultsInput.value = 1;
+childrenInput.value = 0;
+});
+ntnBtn.addEventListener('click', () => {
+    if (departInput.value==0 || returnInput.value==0) {
+        alert('Please select both departing and returning dates.');
+        return;
+        
+    }
+let total=Number(adultsInput.value) + Number(childrenInput.value)
+    alert(`Booking Successful!\nFlyfrom:${optInput.value} \nDeparting: ${departInput.value}\nReturning: ${returnInput.value}\nAdults: ${adultsInput.value}\nChildren: ${childrenInput.value}\n totalFamily:${total}`);
+});
+
+function updateTotal() {
+    let total = Number(adultsInput.value) + Number(childrenInput.value);
+    totalDisplay.textContent = total;
+}
+adults.addEventListener("input", updateTotal);
+children.addEventListener("input", updateTotal);
